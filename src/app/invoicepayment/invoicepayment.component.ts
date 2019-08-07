9daeb5219eac4e7a167be67976a6a744be6f03c4
pipo1596 <pierre_alachkar@hotmail.com>
2019-08-07 16:33:54 -0400

    0807 Delivery to Prod2

--- src/app/invoicepayment/invoicepayment.component.ts
+++ src/app/invoicepayment/invoicepayment.component.ts
@@ -178,7 +178,7 @@ export class InvoicePaymentComponent implements OnInit {
       this.pdate.message = "(invalid)"; this.pdate.erlevel = "D"; this.valid = false;
     }
     //Date in Future
-    if(this.valid && !Util.isFutureDate(this.pdate.value)){
+    if(this.valid && !Util.isFutureDate(Util.formatdateDsp('pdate',this.pdate.value))){
       this.pdate.message = "(Only 2 days into the future allowed!)"; this.pdate.erlevel = "D"; this.valid = false;
     }
     if(this.method.value == "") { this.method.message = "(required)"; this.method.erlevel = "D"; this.valid = false; }
@@ -451,6 +451,7 @@ export class InvoicePaymentComponent implements OnInit {
           if (parseInt(day) < 10) 
             day = "0" + day;
           this.pdate.value = now.getFullYear().toString()+'-'+month+'-'+day;
+          this.pdate.value = Util.formatdateDsp('pdate',this.pdate.value);
           //Default Payment
           this.pagedata.body.paymnt.forEach(elem=>{
             if(elem.stat == 'Y') this.method.value = elem.prof;
